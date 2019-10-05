a11aa32ec4f432cd3db6bce9a5559129d2433137
asucarlos <kuwahara.mee@gmail.com>
2019-10-05 11:00:01 -0400

    add percentage and minus function

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ let typedValue = [];
 let operator = null;
 let OperatorPressed = false;
 let ResultAppeared = false;
+let isMinus = false;
 let formula = [];
 
 //get buttons
@@ -11,6 +12,8 @@ const operators = document.querySelectorAll(".operator");
 const numbers = document.querySelectorAll(".number");
 const equal = document.querySelector(".equal");
 const AC = document.querySelector(".AC");
+const percent = document.querySelector(".percent");
+const minus = document.querySelector(".minus");
 
 //operator key
 operators.forEach(elem => {
@@ -19,8 +22,9 @@ operators.forEach(elem => {
     if (!OperatorPressed) {
       OperatorPressed = true;
       formula = formula.concat(typedValue);
-      typedValue.length = 0;
+      typedValue = [];
     }
+    isMinus = false;
   });
 });
 
@@ -43,12 +47,31 @@ equal.addEventListener("click", e => {
   let result = eval(formula.join(""));
   document.querySelector(".monitor").innerHTML = result; //show on monitor
   formula = [result];
-  typedValue.length = 0;
+  typedValue = [];
+  isMinus = false;
 });
 
 //AC
 AC.addEventListener("click", e => {
-  typedValue.length = 0;
+  typedValue = [];
   formula.length = 0;
+  isMinus = false;
   document.querySelector(".monitor").innerHTML = 0; //show on monitor
+});
+
+//percent
+percent.addEventListener("click", e => {
+  typedValue[0] = typedValue[0] / 100;
+  document.querySelector(".monitor").innerHTML = typedValue.join(""); //show on monitor
+});
+
+//minus
+minus.addEventListener("click", e => {
+  isMinus = !isMinus;
+  if (isMinus) {
+    typedValue.splice(0, 0, "-");
+  } else {
+    typedValue.shift();
+  }
+  document.querySelector(".monitor").innerHTML = typedValue.join(""); //show on monitor
 });
